98c9f53a6820da40323f38dbc35b36c89c2db3a0
booldook <booldook@gmail.com>
2019-01-02 13:56:38 +0900

    navigation js 코딩

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,18 @@
 var bar = $(".navs_mo");
+var bar2 = $(".nav_close");
 var nav = $(".navs_mo_sub");
-var wid = nav.width();
-var navChk = false;
+var navWid = nav.width();
 $(window).resize(function(){
-	wid = nav.width();
+	navWid = nav.width();
+	if($(window).width() > 768) navInit();
+	else navMove();
 }).trigger("resize");
+bar.click(navMove);
+bar2.click(navMove);
+function navInit() {
+	nav.css({"left":-navWid+"px"});
+}
+function navMove() {
+	if(nav.position().left == 0) nav.stop().animate({"left": -navWid+"px"}, 500);
+	else nav.stop().animate({"left": 0}, 500);
+}
