54d014dfd0123337269aa6e436fb1b8972b5d80d
booldook <booldook@gmail.com>
2019-01-03 10:48:02 +0900

    banner 디자인 변경 및 js 정리

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,29 @@
+/***** 공통사항 변수 선언 ******/
 var bar = $(".navs_mo");
 var bar2 = $(".nav_close");
 var nav = $(".navs_mo_sub");
 var navWid = nav.width();
+
+/***** 반응형/높이를 위한 resize ******/
 $(window).resize(function(){
+	navInit();	//모바일 네비게이션 가리기
+	banInit();	//배너 Auto height
+}).trigger("resize");
+
+/***** 메인 배너 ******/
+function banInit() {
+	$(".banner_wrap").height($(".banner_wrap > li").height());
+	$(".banner").height($(".banner_wrap > li").height());
+}
+
+/***** 모바일 네비게이션 ******/
+bar.click(navToggle);
+bar2.click(navToggle);
+function navInit() {
 	navWid = nav.width();
 	if($(window).width() > 768) navHide();
 	else navToggle();
-}).trigger("resize");
-bar.click(navToggle);
-bar2.click(navToggle);
+}
 function navHide() {
 	nav.css({"left":-navWid+"px"});
 }
